c9e486aea0d74feddb49b6129f1efacf078a5266
Harshil <harshil.karia.hk@gmail.com>
2024-09-07 16:18:43 +0530

    Learned basics about routes in contollers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    //constructor(private readonly usersService: UsersService) {}
+
+    // @Post()
+    // postUsers(): string{
+    //     return this.usersService.postUsers();
+    // }
+    // @Get()
+    // getUsers(): string{
+    //     return this.usersService.getUsers();
+    // }
+    // @Get()
+    // getUserDetails(): string{
+    //     return this.usersService.getUsersDetails();
+    // }
+    @Get()
+    findAllUsers() {
+        return [];
+    }
+
+    @Get(':id')
+    findUser(@Param('id') id: string){
+        return { 
+            id,
+            message: 'Specific User'
+        };
+    }
+
+    @Post()
+    addUser(@Body() user: {}){
+        return user;
+    }
+
+    @Patch(':id')
+    updateUser(@Param('id') id: string, @Body() userDetails: {}){
+        return { 
+            id,
+            ...userDetails
+        };
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string){
+        return { 
+            id,
+            message: 'delete'
+        };
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    postUsers(): string{
+        return 'POST Users';
+    }
+    getUsers(): string{
+        return 'GET Users';
+    }
+    getUsersDetails(): string{
+        return 'User Details';
+    }
+}
